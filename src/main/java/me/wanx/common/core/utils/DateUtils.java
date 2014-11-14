91da5c1e2b1ca84ca9035b62fee36765fed53dfa
sun-wanx <594566751@qq.com>
2014-11-14 11:45:51 +0800

    添加类注释

--- src/main/java/me/wanx/common/core/utils/DateUtils.java
+++ src/main/java/me/wanx/common/core/utils/DateUtils.java
@@ -2,8 +2,11 @@ package me.wanx.common.core.utils;
 
 /**
  * 
- * @author gqwang
- *
+* @ClassName: DateUtils 
+* @Description: 日期工具类 
+* @author gqwang
+* @date 2014年11月14日 上午11:43:14 
+*
  */
 public class DateUtils {
 
