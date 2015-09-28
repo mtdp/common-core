ede69a9f703130e986397b1802e8c696d57de391
sun-wanx <594566751@qq.com>
2015-09-28 17:48:17 +0800

    添加生成唯一id类

---
+++ src/test/java/me/wanx/common/IdWorkerTest.java
@@ -0,0 +1,41 @@
+package me.wanx.common;
+
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.TimeUnit;
+
+import me.wanx.common.core.utils.IdWorker;
+
+public class IdWorkerTest { 
+	
+    public static void main(String []args){ 
+        IdWorkerTest test = new IdWorkerTest(); 
+        test.test2(); 
+    } 
+
+    public void test2(){ 
+        final IdWorker w = new IdWorker(1,2); 
+        final CyclicBarrier cdl = new CyclicBarrier(100); 
+
+        for(int i = 0; i < 100; i++){ 
+            new Thread(new Runnable() { 
+                @Override 
+                public void run() { 
+                try { 
+                    cdl.await(); 
+                } catch (InterruptedException e) { 
+                    e.printStackTrace(); 
+                } catch (BrokenBarrierException e) { 
+                    e.printStackTrace(); 
+                } 
+                System.out.println(w.nextId());} 
+             }).start(); 
+        } 
+        try { 
+            TimeUnit.SECONDS.sleep(5); 
+        } catch (InterruptedException e) { 
+           e.printStackTrace(); 
+        } 
+
+    } 
+}
