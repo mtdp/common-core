e47b141a23c1d74994587a223b9a03e839494fec
sun-wanx <594566751@qq.com>
2015-11-24 11:34:26 +0800

    更新httpclient工具类

--- src/main/java/me/wanx/common/core/utils/HttpClientUtils.java
+++ src/main/java/me/wanx/common/core/utils/HttpClientUtils.java
@@ -3,8 +3,10 @@ package me.wanx.common.core.utils;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
+import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.HttpStatus;
@@ -61,6 +63,47 @@ public class HttpClientUtils {
 		return result;
 	}
 	
+	public static String httpPost(String url,Map<String,String> params,Map<String,String> headerParams){
+		String result = null;
+		HttpClient client = new HttpClient();
+		PostMethod post = new PostMethod(url);
+		post.addRequestHeader("Content-Type","text/html;charset="+CHARSET);
+		if(headerParams != null && !headerParams.isEmpty()){
+			for(Map.Entry<String, String> en : headerParams.entrySet()){
+				String k = en.getKey();
+				String v = en.getValue();
+				if(!StringUtils.isNotBlank(v)){
+					v = "";
+				}
+				post.addRequestHeader(k, v);
+			}
+		}
+		if(params != null && !params.isEmpty()){
+			for(Map.Entry<String,String> en : params.entrySet()){
+				String key = en.getKey();
+				String value = en.getValue();
+				if(!StringUtils.isNotBlank(value)){
+					value = "";
+				}
+				NameValuePair v = new NameValuePair(key, value);
+				post.addParameter(v);
+			}
+		}
+		try {
+			client.executeMethod(post);
+			if(post.getStatusCode() == HttpStatus.SC_OK){
+				result = post.getResponseBodyAsString();
+			}
+		} catch (HttpException e) {
+			logger.error("httpclient send post error",e);
+		} catch (IOException e) {
+			logger.error("httpclient send post error",e);
+		}finally{
+			post.releaseConnection();
+		}
+		return result;
+	}
+	
 	
 	public static String httpGet(String url,Map<String,String> params){
 		String result = null;
@@ -93,6 +136,57 @@ public class HttpClientUtils {
 			client.executeMethod(get);
 			if(get.getStatusCode() == HttpStatus.SC_OK){
 				result = get.getResponseBodyAsString();
+			}
+		} catch (HttpException e) {
+			logger.error("httpclient send get error",e);
+		} catch (IOException e) {
+			logger.error("httpclient send get error",e);
+		}finally{
+			get.releaseConnection();
+		}
+		return result;
+	}
+	
+	/**
+	 * 返回responseBoby和responseHeader
+	 * @param url
+	 * @param params
+	 * @return
+	 */
+	public static Map<String,Object> httpGetResultMap(String url,Map<String,String> params){
+		Map<String, Object> result = new HashMap<String,Object>();
+		HttpClient client = new HttpClient();
+		StringBuffer sb = new StringBuffer(url);
+		sb.append("?");
+		if(params != null && !params.isEmpty()){
+			for(Map.Entry<String,String> en : params.entrySet()){
+				String key = en.getKey();
+				String value = en.getValue();
+				if(!StringUtils.isNotBlank(value)){
+					value = "";
+				}else{
+					try {
+						value = URLEncoder.encode(value,CHARSET);
+					} catch (UnsupportedEncodingException e) {
+						logger.error("set http get param error",e);
+						sb.append(key).append("=").append(value).append("&");
+						continue;
+					}
+				}
+				sb.append(key).append("=").append(value).append("&");
+			}
+			//删除最后一个 & 符号
+			sb.deleteCharAt(sb.length()-1);
+		}
+		GetMethod get = new GetMethod(sb.toString());
+		get.addRequestHeader("Content-Type","text/html;charset="+CHARSET);
+		try {
+			client.executeMethod(get);
+			if(get.getStatusCode() == HttpStatus.SC_OK){
+				String respBody = get.getResponseBodyAsString();
+				Header[] respHeader = get.getResponseHeaders();
+				result.put("respBody", respBody);
+				result.put("respHeader", respHeader);
 			}
 		} catch (HttpException e) {
 			logger.error("httpclient send get error",e);
